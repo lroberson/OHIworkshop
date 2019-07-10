f03664b5bd091a6d5ee746b006b7209615bf9077
lroberson <leslie.roberson@gmail.com>
2019-07-10 10:49:16 +1000

    data exploration and error checking

--- OHI_scripts/Notes.R
+++ OHI_scripts/Notes.R
@@ -9,3 +9,8 @@ http://ohi-science.org/data-science-training/
 
 # GIT
 ## stage, commit, pull, push
+  
+# random things
+## One of the nice things about dplyr is that the core code is written in C++ 
+## which means it runs a lot faster than many of the base R functions. 
+## You will notice the difference if you ever work with very large data-sets.
